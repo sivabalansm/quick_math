6c83684f58873577fd05c7e22e2b588bc55106d7
test <test@test.com>
2023-04-08 15:44:04 -0400

    better presentation of challenges

--- sheetgen_v2/interactive.py
+++ sheetgen_v2/interactive.py
@@ -7,7 +7,7 @@ def main():
     functions = (gen.csum, gen.cmult, gen.cdiv)
 
     # input template with num index
-    inputs = (("sum", "digits", "numbers"), ("multiplication", "cand digits", "cator digits"), ("division", "quotient digits", "dividend digits"))
+    inputs = (("sum", "digits", "numbers"), ("multiplication", "cand digits", "cator digits", "×"), ("division", "quotient digits", "dividend digits", "÷"))
 
     print("Enter options w/ space seperated values:\n1) Sums\n2) Multiplication\n3) Division")
     nums = input("#> ")
@@ -16,7 +16,7 @@ def main():
     # validity of inputs
     if len(values) > 0 and nums.replace(" ", "").isdigit() and len(values) <= 3 and all(int(num) in range(1, 4) for num in values):
         nums = list(map(int, values))
-        # programming indexes fix
+        # programming indexs fix
         nums = set([num-1 for num in nums])
 
 
@@ -25,7 +25,7 @@ def main():
         check = [False]
         while not all(check):
             check = list()
-            answers = [(keys, input("For the " + inputs[keys][0] + " what are the " + inputs[keys][1] + " and the " + inputs[keys][2] + " respectively")) for keys in nums]
+            answers = [(keys, input("For the " + inputs[keys][0] + " what are the " + inputs[keys][1] + " and the " + inputs[keys][2] + " respectively> ")) for keys in nums]
             for answer in answers:
                 answer = answer[1]
                 if answer.replace(" ", "").isdigit() and all(int(num) > 0 for num in answer.split(" ")) and len(answer.split(" ")) == 2:
@@ -35,22 +35,29 @@ def main():
                     check.append(False)
                     break
         
+        # converting str nums to ints as tuple
         answers = [(keys, tuple(map(int, answer.split(" ")))) for keys,answer in answers]
+
+        # question mode!
+        count = 1
         while 1:
             for keys,answer in answers:
-                print(functions[keys](answer[0], answer[1]).gen())
+                print(f"[{count}] Compute the " + inputs[keys][0] + ":")
+                challenge,solution = functions[keys](answer[0], answer[1]).gen()
+                if keys == 1 or keys == 2:
+                    print(challenge[0], inputs[keys][3], challenge[1])
+                else: 
+                    for num in challenge:
+                        print(num)
+                input("Answer: ")
+                print(solution)
                 input()
-            
-            
-
+                count+=1
 
     else:
         print("Underterminable values detected")
         main()
         
-
-
-
 
 if __name__ == "__main__":
     main()
